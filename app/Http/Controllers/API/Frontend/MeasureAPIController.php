c09215bf5f707df29ce0bc225a968211949858ac
Alberto <aecheverrya@misena.edu.co>
2021-07-28 08:19:13 -0500

    Inicio del primer Web API que captura la trama de las medidas

--- app/Http/Controllers/API/Frontend/MeasureAPIController.php
+++ app/Http/Controllers/API/Frontend/MeasureAPIController.php
@@ -9,6 +9,8 @@ use App\Repositories\Frontend\MeasureRepository;
 use Illuminate\Http\Request;
 use App\Http\Controllers\AppBaseController;
 use Response;
+use App\Models\Backend\Device;
+use App\Models\Backend\DataVariable;
 
 /**
  * Class MeasureController
@@ -53,11 +55,51 @@ class MeasureAPIController extends AppBaseController
      */
     public function store(CreateMeasureAPIRequest $request)
     {
-        $input = $request->all();
+        // $input = $request->all();
 
-        $measure = $this->measureRepository->create($input);
+        // $measure = $this->measureRepository->create($input);
 
-        return $this->sendResponse($measure->toArray(), 'Measure saved successfully');
+        // return $this->sendResponse($measure->toArray(), 'Measure saved successfully');
+
+        $measures = json_decode($request);
+
+        foreach($measures as $measure){
+            if(($measure->codigo_dispositivo != null) || ($measure->codigo_dispositivo != "")){
+                if(($measure->Id_registro != null) || ($measure->Id_registro != "")){
+                    if(($measure->Fecha_reg != null) || ($measure->Fecha_reg != "")){
+                        if(($measure->Hora_reg != null) || ($measure->Hora_reg != "")){
+                            if(($measure->Dato_var1 != null) || ($measure->Dato_var1 != "")){
+                                $codeDevice = $this->getIdDevice($measure->codigo_dispositivo);
+                                if(isset($codeDevice)){
+                                    $codeVariable = $this->getIdVariable($measure->Id_registro);
+                                    if(isset($codeVariable)){
+                                        if( $this->insertMeasure($codeDevice, $codeVariable, $measure->Fecha_reg, $measure->Hora_reg, $measure->Dato_var1) == 200){
+                                            return $this->sendResponsegetIdDevice(200, 'Ok');                          
+                                        } else {
+                                            return $this->sendResponsegetIdDevice(501, 'Error de Base de Datos');
+                                        }
+                                    } else {
+                                        return $this->sendResponse(501, 'Error: La variable ' . $measure->Id_registro . 'No es valida.');
+                                    }
+                                } else {
+                                    return $this->sendResponse(501, 'Error: El código ' . $measure->codigo_dispositivo . 'No es valido.');
+                                }
+                            } else {
+                                return $this->sendResponse(501, 'Error: No hay dato de la variable. ' . $measure->Id_registro);
+                            }
+                        } else {
+                            return $this->sendResponse(501, 'Error: No hay hora de registor para la variable. ' . $measure->Id_registro);                            
+                        }
+                    } else {
+                        return $this->sendResponse(501, 'Error: No hay fecha de registor para la variable. ' . $measure->Id_registro); 
+                    }
+                } else {
+                    return $this->sendResponse(501, 'Error: No hay variable para el dispositivo. ' . $measure->codigo_dispositivo); 
+                }
+            } else {
+                return $this->sendResponse(501, 'Error: No hay código de dispositivo. ' . $measure->codigo_dispositivo); 
+            }
+        }
     }
 
     /**
@@ -127,5 +169,44 @@ class MeasureAPIController extends AppBaseController
         $measure->delete();
 
         return $this->sendSuccess('Measure deleted successfully');
+    }
+
+    /**
+     * This function get the id that match with the input parameter.
+     */
+    private function getIdDevice($codeDevice){
+        $codDevice = Device::select('id')
+                              ->where('device_code', $codeDevice)
+                              ->get();
+        return $codDevice;
+    }
+
+    /**
+     * This function get the id that match with the input parameter.
+     */    
+    private function getIdVariable($codeVariable){
+        $codVariable = DataVariable::select('id')
+                              ->where('name ', $codeVariable)
+                              ->get();
+        return $codVariable;
+    }    
+
+    /**
+     * This function insert the measere's data.
+     */
+    private function insertMeasure($codeDevice, $codeVariable, $recordDate, $recordHour, $variableData){
+        try {
+            Measure::insert([
+                'date' => $recordDate,
+                'hour' => $recordHour,
+                'data' => $variableData,
+                'device_id' => $codeDevice,
+                'data_variable_id' => $codeVariable
+            ]); 
+        } catch (\Throwable $th) {
+            return 501;
+        }
+
+        return 200;
     }
 }
